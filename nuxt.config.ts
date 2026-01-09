5cb9da1c950ded043805e61ddf916ee377f9764f
Rastuharem <Rastuharem@gmail.com>
2026-01-10 00:34:49 +0300

    feat: added tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: "2026-01-10",
   modules: ["@nuxt/content", "@nuxt/image", "@nuxt/eslint", "@vueuse/nuxt"],
+  css: ["./app/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
   devtools: { enabled: true },
-  compatibilityDate: "2024-04-03",
 });
