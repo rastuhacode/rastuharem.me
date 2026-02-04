5d62457f7c1e1293fba1ad8461b3b8b620ae4c9b
Rastuharem <Rastuharem@gmail.com>
2026-02-04 18:40:34 +0300

    chore: update .gitignore to remove IDE folders, add VS Code settings and extensions for improved development experience, and enhance RSwitch styles for better state management

--- app/components/ui/switch/RSwitch.vue
+++ app/components/ui/switch/RSwitch.vue
@@ -22,7 +22,9 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits);
     :class="
       cn(
         'peer inline-flex h-[1.15rem] w-8 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none',
-        'data-[state=checked]:bg-foreground data-[state=unchecked]:bg-input',
+        'data-[state=unchecked]:bg-ring data-[state=checked]:bg-foreground',
+        'dark:data-[state=unchecked]:bg-input',
+
         'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
         'disabled:cursor-not-allowed disabled:opacity-50',
         props.class
